c752539e8a9e3182ce569ff75ca7d1f6db007d62
Jina W <70620580+hkl690@users.noreply.github.com>
2024-08-26 11:45:37 -0500

    added fifth_most_viewed heap exercise

---
+++ PythonPractice/FifthMostViewed.py
@@ -0,0 +1,15 @@
+import heapq
+
+# Given a list of webpages and the number of times each webpage has been viewed, 
+# return the 5th most viewed webpage.
+
+def fifth_most_viewed(webpages):
+    min_heap = []
+    for page, views in webpages:
+        heapq.heappush(min_heap, (views, page))
+        if len(min_heap) > 5:
+            heapq.heappop(min_heap)
+    return heapq.heappop(min_heap)[1]
+
+webpages = [("page1", 100), ("page2", 200), ("page3", 300), ("page4", 400), ("page5", 500), ("page6", 600)]
+print(fifth_most_viewed(webpages))
