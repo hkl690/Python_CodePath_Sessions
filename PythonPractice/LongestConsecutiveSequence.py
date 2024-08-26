0b14868153850b03357c3a1b2eb7f55228d459f6
Jina W <70620580+hkl690@users.noreply.github.com>
2024-08-25 20:38:44 -0500

    had to look at their solution because it says this is done in O(N) which it seems like it isn't with all the loops

--- PythonPractice/LongestConsecutiveSequence.py
+++ PythonPractice/LongestConsecutiveSequence.py
@@ -1,1 +1,23 @@
-enumerate()
+from unicodedata import numeric
+
+
+def longestConsecutiveSequence(nums):
+    longestStreak = 0
+    dict = set(nums)
+    
+    for num in dict:
+        # If current number has no previous number, then start counting from this number
+        if num-1 not in dict:
+            currentNum = num
+            currentStreak = 1
+            # While the set contains currentNum + 1, increment currentNum and currentStreak
+            while currentNum + 1 in dict:
+                currentNum += 1
+                currentStreak += 1
+            # After each while loop, check and set longestStreak
+            longestStreak = max(longestStreak, currentStreak)
+            
+    return longestStreak
+
+print(longestConsecutiveSequence([100,4,200,1,3,2]))
+print(longestConsecutiveSequence([0,3,7,2,5,8,4,6,0,1]))
