af1c68b87be01691eeeadd2c0befcff881f15f39
Jina W <70620580+hkl690@users.noreply.github.com>
2024-09-18 10:51:15 -0500

    problem 6 completed

--- PythonPractice/unit1session1_sept.py
+++ PythonPractice/unit1session1_sept.py
@@ -76,3 +76,15 @@ print(f"Hunny jars sum total: {sum_honey(hunny_jars1)}") # output 14
 
 hunny_jars2 = []
 print(f"Hunny jars sum total: {sum_honey(hunny_jars2)}") # output 0
+
+# Problem 6: Double Trouble
+# Help Winnie the Pooh double his honey! Write a function doubled() that accepts a list of integers
+# hunny_jars as a parameter and multiplies each element in the list by two. Return the doubled list.
+
+def doubled(hunny_jars):    
+    for i in range(len(hunny_jars)):
+        hunny_jars[i] *= 2
+    return hunny_jars
+    
+hunny_jars = [1, 2, 3]
+print(doubled(hunny_jars))
