cb44c36920b3919497264e39db994b4628bcac95
Jina W <jina.wilde@my.utsa.edu>
2024-09-26 08:15:50 -0500

    practicing unit 2 session 2 standard B

--- PythonPractice/unit2sessions_sept.py
+++ PythonPractice/unit2sessions_sept.py
@@ -287,3 +287,49 @@ print(roman_to_int("LVIII")) # 58   <- IIIVL in reverse
 print(roman_to_int("MCMXCIV")) # 1994   <-- VICXMCM in reverse
 print(roman_to_int("IV")) #4
 print(roman_to_int("XI")) #11
+
+# Unit 2 Session 2 Standard B 
+# 
+# Problem 1: Filter Destinations
+# You're planning an epic trip and have a dictionary of destinations mapped to their respective rating scores. Your goal is to visit only the best-rated destinations. Write a function that takes in a dictionary destinations and a rating_threshold as parameters. The function should iterate through the dictionary and remove all destinations that have a rating strictly below the rating_threshold. Return the updated dictionary.
+
+def remove_low_rated_destinations(destinations, rating_threshold):
+    keys_to_remove = [key for key, value in destinations.items() if value < rating_threshold]
+    print(keys_to_remove)
+    for key in keys_to_remove:
+        destinations.pop(key)
+    return destinations
+
+destinations = {"Paris": 4.8, "Berlin": 3.5, "Addis Ababa": 4.9, "Moscow": 2.8}
+destinations2 = {"Bogota": 4.8, "Kansas City": 3.9, "Tokyo": 4.5, "Sydney": 3.0}
+
+print(remove_low_rated_destinations(destinations, 4.0)) # {"Paris": 4.8, "Addis Ababa": 4.9}
+print(remove_low_rated_destinations(destinations2, 4.9)) # {}
+
+# Problem 2: Unique Travel Souvenirs
+# As a seasoned traveler, you've collected a variety of souvenirs from different destinations. You have an array of string souvenirs, where each string represents a type of souvenir. You want to know if the number of occurrences of each type of souvenir in your collection is unique.
+
+# Write a function that takes in an array souvenirs and returns True if the number of occurrences of each value in the array is unique, or False otherwise.
+
+def unique_souvenir_counts(souvenirs):
+
+
+souvenirs1 = ["keychain", "hat", "hat", "keychain", "keychain", "postcard"]
+souvenirs2 = ["postcard", "postcard", "postcard", "postcard"]
+souvenirs3 = ["keychain", "magnet", "hat", "candy", "postcard", "stuffed bear"]
+
+print(unique_souvenir_counts(souvenirs1))  
+print(unique_souvenir_counts(souvenirs2)) 
+print(unique_souvenir_counts(souvenirs3))
+# Expected Outputs:
+# True
+# Example 1 Explanation: The value "keychain" has 3 occurrences, "hat" has 2 
+# and "postcard" has 1. No two values have the same number of occurrences.
+
+# True
+# Example 2 Explanation: The value "postcard" appears 4 times There's only one count (4), 
+# which is technically unique, so this should also return True.
+
+# False
+# Example 3 Explanation: Each item appears 1 time All counts are 1, which is not unique, 
+# so this should return False.
