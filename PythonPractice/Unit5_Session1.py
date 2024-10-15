730b14221913579274b6957ec8771ba592059990
Jina W <jina.wilde@my.utsa.edu>
2024-10-15 14:03:03 -0500

    updates

--- PythonPractice/Unit5_Session1.py
+++ PythonPractice/Unit5_Session1.py
@@ -1,4 +1,4 @@
-# Problem 1: Building a Playlist
+﻿# Problem 1: Building a Playlist
 # The assignment statement to the top_hits_2010s variable below creates the linked list Uptown Funk -> Party Rock Anthem -> Bad Romance. Break apart the assignment statement into multiple lines with one call to the Node constructor per line to recreate the list.
 
 
@@ -26,7 +26,7 @@ top_hits_2010s = SongNode("Uptown Funk", node2)
 print_linked_list(top_hits_2010s)
 # Example Output: Uptown Funk -> Party Rock Anthem -> Bad Romance
 
-# ?? Hint: Nested Constructors
+# 💡 Hint: Nested Constructors
 # This problem requires you to be familiar with nesting constructors. The Node class below accepts two parameters:
 
 # the value of the Node object.
@@ -57,7 +57,7 @@ class SongNode:
         self.next = next
 
 # For testing
-def print_linked_list(node):
+def print_linked_list(node):    
     current = node
     while current:
         print((current.song, current.artist), end=" -> " if current.next else "")
@@ -65,6 +65,8 @@ def print_linked_list(node):
     print()
 
 def get_artist_frequency(playlist):
+    if not playlist:
+        return {}
     artist_frequency = {}
     current = playlist
     
