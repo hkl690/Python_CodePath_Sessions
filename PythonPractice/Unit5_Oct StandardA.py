416a289cb7fd08e62c7bed7f0f4074afe9d6a0c9
Jina W <jina.wilde@my.utsa.edu>
2024-10-17 13:44:04 -0500

    codepath unit 5 session 2 exercises

--- PythonPractice/Unit5_Oct StandardA.py
+++ PythonPractice/Unit5_Oct StandardA.py
@@ -589,3 +589,69 @@ print(print_linked_list(merged_head))  # Expected output: 1 -> 2 -> 3 -> 4 -> 5 
 #         else:
 #             current = current.next
 #     return temp_head.next
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def delete_item(head, item):
+    if not head:
+        return None
+
+    if head.value == item:
+        return head.next
+
+    current = head
+    while current.next and current.next.value != item:
+        current = current.next
+    if current.next:
+        current.next = current.next.next
+
+    return head
+
+slingshot = Node("Slingshot")
+peaches = Node("Peaches")
+beetle = Node("Scarab Beetle")
+slingshot.next = peaches
+peaches.next = beetle
+
+# Linked List: slingshot -> peaches -> beetle
+print_linked_list(delete_item(slingshot, "Peaches"))
+
+# Linked List: slingshot -> beetle
+print_linked_list(delete_item(slingshot, "Triceratops Torso"))
+
+class Node:
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+def print_reverse(tail):
+    current = tail
+    while current:
+        if current.prev:
+            print(current.value, end=" ")
+        else:
+            print(current.value)
+        current = current.prev
+
+isabelle = Node("Isabelle")
+kk_slider = Node("K.K. Slider")
+saharah = Node("Saharah")
+isabelle.next = kk_slider
+kk_slider.next = saharah
+saharah.prev = kk_slider
+kk_slider.prev = isabelle
+
+# Linked List: Isabelle <-> K.K. Slider <-> Saharah
+print_reverse(saharah)
+# Expected Output: "Saharah K.K. Slider Isabelle"
